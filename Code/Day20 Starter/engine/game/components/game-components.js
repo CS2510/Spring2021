7f769dfdb33f4b2775de90cf6baa461b387c908b
B. Ricks <bricks@unomaha.edu>
2021-03-31 11:27:27 -0500

    Add Day 20 Starter

---
+++ Code/Day20 Starter/engine/game/components/game-components.js
@@ -0,0 +1,2 @@
+export {default as CollisionControllerComponent} from "./collision-controller-component.js"
+export {default as MainControllerComponent} from "./main-controller-component.js"
---
+++ Code/Day20 Starter/engine/game/scenes/game-scenes.js
@@ -0,0 +1,1 @@
+export {default as CollisionScene} from "./collision-scene.js"
