a7ede8155cf6567cf62deed0ea924feb623032fe
B. Ricks <bricks@unomaha.edu>
2021-04-05 16:04:27 -0500

    Add boilerplate

---
+++ Code/Day23 Wrapping/game/components/game-components.js
@@ -0,0 +1,3 @@
+export {default as MainControllerComponent} from "./main-controller-component.js"
+export {default as StartControllerComponent} from "./start-controller-component.js"
+export {default as KeyboardMoveComponent} from "./keyboard-move-component.js"
