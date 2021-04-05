09fc4b7bcbcb0fe2175d3de8ebfaffe938dfcdee
B. Ricks <bricks@unomaha.edu>
2021-04-05 12:55:08 -0500

    Start Day 22

---
+++ Code/Day22 Ref/game/scenes/collision-scene.js
@@ -0,0 +1,58 @@
+export default {
+  name: "CollisionScene", //Scene name
+  children: [ //Game objects in the scene
+    {
+      gameObject: {
+        name: "MainCamera", components: [
+          { name: "WorldCameraComponent" },
+        ]
+      }
+    },
+    {
+      gameObject: {
+        name: "ScreenCamera", components: [
+          { name: "ScreenCameraComponent" }
+        ], children: [
+          {
+            gameObject: {
+              name: "ScreenText",
+              components: [
+                {
+                  name: "ScreenTextComponent",
+                  args: ["Collision Scene", { alignment: "left", justification: "bottom" }]
+                }
+              ]
+            },
+            x: 40, y: 40
+          },
+        ]
+      }
+    },
+    {
+      gameObject: {
+        name: "Empty",
+        components: [
+         {name: "MainControllerComponent"}
+        ]
+      }
+    },
+    {
+      gameObject:{
+        name:"CircleGameObject",
+        components:[
+          {name: "DrawGeometryComponent", args:["brown"]},
+          {name: "CircleGeometryComponent", args:[100]}
+        ]
+      }
+    },
+    {
+      gameObject:{
+        name:"RectangleGameObject",
+        components:[
+          {name: "DrawGeometryComponent", args:["yellow"]},
+          {name: "RectangleGeometryComponent", args:[50, 50]}
+        ]
+      },x:100,y:100
+    }
+  ]
+}
---
+++ Code/Day22 Starter/game/components/game-components.js
@@ -0,0 +1,2 @@
+export {default as MainControllerComponent} from "./main-controller-component.js"
+export {default as StartControllerComponent} from "./start-controller-component.js"
